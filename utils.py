ece38394410fab3d6ccac2c7dbc8f72206da29c6
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2023-05-19 08:09:23 -0400

    Add utils

---
+++ utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def sine(angle):
+    # convert to radians
+    return np.sin(np.radians(angle))
+
+def is_floatable(v):
+    try:
+        float(v)
+        return True
+    except ValueError:
+        return False
