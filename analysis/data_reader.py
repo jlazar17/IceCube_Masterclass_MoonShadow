c67c2152a64c14fef2fd07b86fe91a18f66d87f8
Jeffrey Lazar <jeff.p.lazar@gmail.com>
2023-05-19 11:43:30 -0400

    .

--- analysis/data_reader.py
+++ analysis/data_reader.py
@@ -32,7 +32,7 @@ class DataReader:
         self._azimuth = None
         self._direction = None
         self._time = None
-        self._photon_info = ak.from_parqet(filename)
+        self._photon_info = ak.from_parquet(filename)
         self._events = read_events(filename)
         self._index = 0
         
